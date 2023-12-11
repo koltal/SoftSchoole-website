3567d1b500226774db7c87d005367d862375b313
1musty <mustapha@clivealliance.com>
2023-12-11 11:27:30 +0100

    done with features and usecase

--- src/Screens/Home/Main.jsx
+++ src/Screens/Home/Main.jsx
@@ -35,13 +35,6 @@ const Main = () => {
           <p className=" font-semibold">adidas</p>
         </div>
 
-        {/* <div className="w-[43%] sm:w-full sm:mb-4 relative p-0 bg-white overflow-hidden rounded-xl shadow-shift-shadow sm:shadow-0">
-              <img className="w-full object-none sm:hidden"  src={mainPic} alt="main" />
-              <img className="w-full hidden object-none sm:inline-block"  src={mainMobilePic} alt="main" />
-
-              <div className="absolute sm:hidden top-0 left-0 w-full h-full bg-[#F4F4F4] opacity-30"/>
-
-        </div> */}
         <div className="50% relative">
           <img className="w-full sm:hidden"  src={mainPic} alt="main" />
           <img className="w-full hidden object-none sm:inline-block"  src={mainMobilePic} alt="main" />
