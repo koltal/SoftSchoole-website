9a81ed1fd073d7531458027e98853665e7ecf40e
1musty <mustapha@clivealliance.com>
2024-01-14 10:12:52 +0100

    added scroll up animation

--- src/Component/Nav/Nav.jsx
+++ src/Component/Nav/Nav.jsx
@@ -1,6 +1,14 @@
 import { useLocation, Link } from "react-router-dom";
 import { SlArrowDown } from "react-icons/sl";
 import { useState } from "react";
+import { animateScroll as scroll } from "react-scroll";
+
+const handleScrollToTop = () => {
+  scroll.scrollToTop({
+    duration: 500, // Set the duration of the scroll animation
+    smooth: "easeInOutQuart", // Set the easing function for the animation
+  });
+};
 
 const Nav = () => {
   return (
@@ -38,7 +46,7 @@ const NavItem = ({ text, active, list }) => {
   const path = `/${pathname.split("/")[1]}`;
 
   return (
-    <Link to={list === undefined && active}>
+    <Link to={list === undefined && active} onClick={handleScrollToTop}>
       <div className="flex flex-col cursor-pointer w-full">
         <div className="text-[16px] font-medium flex items-center gap-1 hover:text-primary">
           <span>{text}</span>
@@ -80,7 +88,7 @@ const NavList = ({ children, list }) => {
       {openPopper && (
         <div className="absolute shadow-shift-shadow-small px-3 py-2 left-0 mt-8 top-0 w-48 bg-white min-h-[50px]">
           {list.map((val, ind) => (
-            <Link key={ind} to={val?.link}>
+            <Link key={ind} to={val?.link} onClick={handleScrollToTop}>
               <div
                 onMouseEnter={() => setIndex(ind)}
                 onMouseLeave={() => setIndex(null)}
